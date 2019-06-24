279aa10b2f029e13d3eeaba6dad665d965871b2a
Joseph Herlant <aerostitch@debian.org>
2019-06-23 22:50:23 -0700

    Add support for listing and pulling users

--- slab/users.go
+++ slab/users.go
@@ -1,5 +1,10 @@
 package slab
 
+import "context"
+
+// UserService is an implementation of the service to interact with the users
+type UserService service
+
 // User represents a given slab user
 type User struct {
 	ID            string    `json:"id"`
@@ -18,4 +23,55 @@ type User struct {
 type Image struct {
 	Original string `json:"original"`
 	Thumb    string `json:"thumb"`
+}
+
+// List retrieves all the users available in the organization including their details
+func (p *UserService) List() (*[]User, error) {
+	query := `{
+		organization {
+			users{
+				id,
+				name,
+				description,
+				email,
+				title,
+				type,
+				avatar{original, thumb},
+				insertedAt,
+				deactivatedAt,
+				updatedAt
+			}
+		}
+	}`
+	var resp struct {
+		Organization *Organization `json:"organization"`
+	}
+	err := p.client.Do(context.Background(), query, nil, &resp)
+	return resp.Organization.Users, err
+}
+
+// Get retrieves the details of a specific user including its content
+func (p *UserService) Get(id string) (*User, error) {
+	query := `
+    query ($id: ID){
+		user(id: $id){
+			id,
+			name,
+			description,
+			email,
+			title,
+			type,
+			avatar{original, thumb},
+			insertedAt,
+			deactivatedAt,
+			updatedAt
+		}
+    }`
+	var resp struct {
+		User *User `json:"user"`
+	}
+	vars := map[string]interface{}{"id": id}
+	err := p.client.Do(context.Background(), query, vars, &resp)
+	return resp.User, err
+
 }
